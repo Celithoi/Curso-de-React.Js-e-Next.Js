d460f161ee4cd328bbff4dd213e9cfb27fa70133
Marcelo Rocha Cardoso <mhrcardoso@gmail.com>
2025-04-06 14:51:00 -0300

    basico chato

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Heading } from "./components/Heading";
+
 function App() {
   console.log("oi");
-  return <h1>Olá mundo(do APP)</h1>;
+  return (
+    <>
+      <h1>Olá mundo(do APP)</h1>
+      <Heading />
+    </>
+  );
 }
 
 export { App };
